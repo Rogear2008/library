238c18e6f3e3499aea06526ffb5c2f834580a6d5
rogear2008 <rogear@qq.com>
2020-04-24 17:51:42 +0800

    update

---
+++ library-mvc/src/main/java/com/rogear/library/controller/UploadPic.java
@@ -0,0 +1,91 @@
+package com.rogear.library.controller;
+
+import org.apache.commons.io.FileUtils;
+import org.aspectj.util.FileUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * Created by Rogear on 2020/4/24
+ **/
+public class UploadPic {
+
+    @RequestMapping("/uploadPic")
+    public String uploadPic(@RequestParam("file")MultipartFile file) throws IOException {
+        String dir = "/images";
+        String type = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."),file.getOriginalFilename().length());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        String preName = simpleDateFormat.format(new Date());
+        StringBuilder fileName = new StringBuilder(preName);
+        Random random = new Random();
+        if (type.equals(".jpg") || type.equals(".jpeg") || type.equals(".png")){
+            fileName = fileName.append("_"+random.nextInt(10)).append(type);
+        } else {
+            return "0";
+        }
+        FileUtils.writeByteArrayToFile(new File(dir,fileName.toString()),file.getBytes());
+        return fileName.toString();
+    }
+
+    @RequestMapping(value = "/uploadHeadPic"
+            //, method = RequestMethod.POST
+            //, produces = "application/json; charset=utf-8"
+    )
+//    @ResponseBody
+    public Object uploadHeadPic(@RequestParam() MultipartFile file, HttpServletRequest request) {
+        //在这里面文件存储的方案一般是：收到文件→获取文件名→在本地存储目录建立防重名文件→写入文件→返回成功信息
+        //如果上面的步骤中在结束前任意一步失败，那就直接失败了。
+        FileOutputStream out=null;
+        if (null == file || file.isEmpty()) {
+            System.out.println("文件为空");
+        }else{
+            System.out.println("上传成功");
+            //这里以用户ID作为文件夹
+            int uid = (Integer) request.getSession().getAttribute("userid");
+            //创建一个文件夹，网上代码很多
+            String url = "/images";
+            try {
+                //获得二进制流并输出
+                byte[] f = file.getBytes();
+                out = new FileOutputStream(url+file.getOriginalFilename());
+                out.write(f);
+
+            } catch (IOException e) {
+                System.out.println("上传失败");
+            }finally {
+                // 完毕，关闭所有链接
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    System.out.println("关闭流失败");
+                }
+            }
+
+        }
+
+        return null;
+    }
+
+
+    @RequestMapping(value="/upload", method=RequestMethod.POST)
+//    @ResponseBody
+    public String uploadFile(HttpServletRequest request,
+                                          @RequestParam MultipartFile uploadFile){
+        Map<String,  Object> resultMap = new HashMap<>();
+        return "123";
+    }
+
+}
---
+++ library-mvc/src/main/webapp/views/upload.jsp
@@ -0,0 +1,53 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2020/4/24
+  Time: 14:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>Title</title>
+    <jsp:include page="jqueryHead.jsp"/>
+</head>
+<body>
+<div>
+    <form method="post" id="file" action="" enctype="multipart/form-data">
+            <h3>选择一个文件:</h3>
+            <input id="excelFile" type="file" name="uploadFile" />
+            <br/><br/>
+            <input type="button" value="上传" οnclick="javascript:uploadFiles()"/>
+        <a href="javascript:uploadFiles()" class="easyui-linkbutton">上传</a>
+    </form>
+</div>
+</body>
+<script type="text/javascript">
+    function uploadFiles(){
+// 	var uploadFile = $('#excelFile').get(0).files[0];
+        var uploadFile = new FormData($("#file")[0]);
+        console.log(uploadFile);
+        if("undefined" != typeof(uploadFile) && uploadFile != null && uploadFile != ""){
+            $.ajax({
+                url:'${pageContext.request.contextPath}/upload',
+                type:'POST',
+                data:uploadFile,
+                async: false,
+                cache: false,
+                contentType: false, //不设置内容类型
+                processData: false, //不处理数据
+                success:function(data){
+                    console.log(data);
+                    alert(data.msg);
+                },
+                error:function(){
+                    alert("上传失败！");
+                }
+            })
+        }else {
+            alert("选择的文件无效！请重新选择");
+        }
+    }
+</script>
+</html>
